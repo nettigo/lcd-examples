6d4e582c37d781553294f8c391a58273e096a67b
Witold Rugowski <rugowski@nhw.pl>
2024-02-12 15:30:49 +0100

    initial version

---
+++ src/libGFX.h
@@ -0,0 +1,31 @@
+//
+// Created by viciu on 2/9/24.
+//
+
+#ifndef ST7796_4_CALE_LIBGFX_H
+#define ST7796_4_CALE_LIBGFX_H
+#include <Arduino_GFX_Library.h>
+//#include "setupDebug.h"
+Arduino_DataBus *bus = new Arduino_HWSPI(D3 /* DC */, D2 /* CS */);
+Arduino_GFX *gfx = new Arduino_ST7796(bus, D4 /* RST */);
+#include "ntg_napis.h"
+void setup(){
+    Serial.begin(115200);
+    Serial.println("\nSTART");
+    gfx->begin(80000000);
+    gfx->setRotation(1);
+    gfx->fillScreen(BLACK);
+    gfx->println("Hello World!");
+    Serial.println("Hello");
+    delay(1000);
+//    gfx->displayOff();
+//    gfx->draw16bitRGBBitmap(0, 10, ntg_napis, NTG_NAPIS_WIDTH, NTG_NAPIS_HEIGHT);
+    Serial.println("Hello2");
+//    gfx->displayOn();
+
+};
+
+void loop(){
+
+};
+#endif //ST7796_4_CALE_LIBGFX_H
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by viciu on 2/9/24.
+//
+#include <SPI.h>
+#ifdef ADAFRUIT
+#include "libGFX.h"
+#else
+#include "etft.h"
+#endif
+
